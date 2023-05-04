04ded66bb3a11513f286bd77c44ec7cf1749e20d
Opus28 <110404695+Opus28@users.noreply.github.com>
2023-05-04 17:23:47 -0500

    added mason and tabline without configs

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,12 +1,13 @@
 -- View
-vim.opt.number = true           --> show line numbers
-vim.opt.relativenumber = true   --> show relative line numbers
-vim.opt.cursorline = true       --> highlight current line
+vim.opt.number = true               --> show line numbers
+vim.opt.relativenumber = false      --> show relative line numbers
+vim.opt.cursorline = true           --> highlight current line
+-- vim.opt.cursorlineopt = "number"    --> highlight current line
 
 -- Indentation and so such
-vim.opt.expandtab = true        --> tab inserts spaces
-vim.opt.tabstop = 4             --> tab width is 4 spaces
-vim.opt.shiftwidth = 4          --> ">" & "<" shift by four
-vim.opt.shiftround = true       --> round shift indentation
+vim.opt.expandtab = true            --> tab inserts spaces
+vim.opt.tabstop = 4                 --> tab width is 4 spaces
+vim.opt.shiftwidth = 4              --> ">" & "<" shift by four
+vim.opt.shiftround = true           --> round shift indentation
 
-vim.opt.showcmd = true          --> show last command at last line
+vim.opt.showcmd = true              --> show last command at last line
